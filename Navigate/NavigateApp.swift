764ea41c5ed980c4295e921fd8feae7580fb7bcc
Lily Mosisa <lilymosisa13@gmail.com>
2025-04-23 00:38:00 -0400

    Initial Commit

---
+++ Navigate/NavigateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigateApp.swift
+//  Navigate
+//
+//  Created by Lily Mosisa on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigateTests/NavigateTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigateTests.swift
+//  NavigateTests
+//
+//  Created by Lily Mosisa on 4/23/25.
+//
+
+import Testing
+@testable import Navigate
+
+struct NavigateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
